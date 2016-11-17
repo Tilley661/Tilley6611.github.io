390c2d8a164225068fc93bfb65233c7d7b341a59
Daniel Tilley <Tilley661>
2016-11-17 21:35:35 +0000

    write to debug

--- js/jq/mainFun.js
+++ js/jq/mainFun.js
@@ -1,3 +1,14 @@
 //Main functions to call to every page
 
-alert("hello");
+
+
+function printLine(str){
+	if (str === undefined){
+		str = "No debug sent";
+	}else{
+		$("#debug").HTML(str);
+	}
+}
+
+printLine("testing debug");
+
