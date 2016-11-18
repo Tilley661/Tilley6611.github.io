74eba8b115b9691748f9d43af34ea94720e01cd3
Daniel Tilley <Tilley661>
2016-11-18 20:56:23 +0000

    edit debug section

--- js/jq/mainFun.js
+++ js/jq/mainFun.js
@@ -2,17 +2,22 @@
 
 
 //my debug function
-function debugPrint(str, clear){
+function debugPrint(str, clear, dynCSS){
+	var a = Math.random();
 	if (str === undefined){
 		str = "No debug sent";
 	}
-	if (clear == true){
+	if (clear === true){
 		$("#debug").html("");
 	}
 	var p = document.createElement("p");
 	var t = document.createTextNode(str);
 	p.appendChild(t);
+	p.id
+	if (bold !== undefined && bold === true){
+		p.style.cssText = dynCSS;
+	}
 	$("#debug").append(p);
 }
 
-debugPrint("Debug Section:", false);
+debugPrint("Debug Section:", false, "font-weight:bold;");
