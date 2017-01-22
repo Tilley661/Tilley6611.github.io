f3d9cfe5484424d4efe6697d33cf9032df74cd0e
Daniel Tilley <Tilley661>
2017-01-22 13:59:13 +0000

    good luck

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -8,7 +8,10 @@
 var wH = $( window ).height();             //current window height
 var wW = $( window ).width();             //current window width
 var qL = $('#main-nav ul li').length;      //how many links
-var cP = 1;                                          //current page
+var cP = 1;                                         //current page
+var kFK = 1;                                       //keyframe counter
+
+
 
 //on resize
 $( window ).resize(function() {
@@ -27,30 +30,74 @@ $( window ).resize(function() {
 
 //get curr position of object to move
 
-function moveTopLeft(link){
+function moveToTitle(link){
 var curr = $("#main-nav ul li[data-menu-index='" + link +"']");     //make a curr object
 var currOffset = curr.offset();                                                     //make offset object
 var endPos = 50;                                                                      //50px from top
 var calcMove = currOffset.top - endPos;                                        //calculate move amount
 
+
 //translate using css
+curr.addClass( ".move-to-title" );
+//need to remove in moving to nav
+
 curr.css("transform", "translate(0px, -" + calcMove + "px)");
 
+
+
+	//create dynamic keyframe and add a number to the end
+	$.keyframe.define([{
+		name: 'move-to-title-' + kFK,
+		'50%': {'transform': 'translate(0px,-' + calcMove + 'px)'},
+		'100%': {'transform': 'translate(200px,-' + calcMove + 'px)'}
+	}]);
+	
+	
+	//add class to link to actually move it
+	curr.css( 
+				{	
+					'animation-fill-mode': 'forwards',
+					'animation-duration': '1s',
+					'animation-name': 'move-to-title-' + kFK 
+				});
+	//add integer to to create more later	
+	kFK = kFK + 1;
+	
 }
 
-moveTopLeft(cP); //move certain link
 
 
 //add listener to check if anything has been clicked in navigation
 $("#main-nav ul li a").click(function(){
-	var clicked = $(this).parent().attr('data-menu-index')
-	 if (!isNaN(clicked) == true){
+	var clicked = $(this).parent().attr('data-menu-index')      //get index from data tag
+	
+	                                                                                 //check is number
+	if (!isNaN(clicked) == true){
 		 alert('clicked is numeric');
-		//moveToTitle(clicked);
+		 moveToTitle(clicked);
 	 }else{
 		 alert('There is an issue.');
+		 return;
 	 }
+	 
+
+	 
+	 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//move current page to to title
+MoveToTitle(cP);
 
 
 //how to locate menu item
