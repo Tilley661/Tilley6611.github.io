8f4466a99f4ad6dc21f603a11013e9bc61fcf8e9
Daniel Tilley <Tilley661>
2017-01-22 09:50:28 +0000

    adding nav

---
+++ js/jq/navFun.js
@@ -0,0 +1,43 @@
+//Main functions to call to every page
+
+
+
+//get init vars
+
+//screen variables
+var wH = $( window ).height();
+var wW = $( window ).width();
+
+
+//on resize
+$( window ).resize(function() {
+	debugPrint("Window hase resized", false, false);
+});
+
+
+
+
+/* //my debug function
+function debugPrint(str, clear, bold){
+	var a = Math.random();
+	if (str === undefined){
+		str = "No debug sent";
+	}
+	if (clear === true){
+		$("#debug").html("");
+	}
+	var p = document.createElement("p");
+	var t = document.createTextNode(str);
+	p.appendChild(t);
+	if (bold !== undefined && bold === true){
+		p.style.cssText = "font-weight:bold;";
+	}
+	$("#debug").append(p);
+}
+
+debugPrint("Debug Section:", false, true);
+
+
+ */
+
+
