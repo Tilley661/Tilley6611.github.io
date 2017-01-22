cec91b009980c30062f85da73c9337ff9120bf2c
Daniel Tilley <Tilley661>
2017-01-22 13:13:59 +0000

    this

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -42,7 +42,7 @@ moveTopLeft(cP); //move certain link
 
 
 $("#main-nav ul li a").click(function(){
-	alert($(this).parent.html());
+	alert($(this).html());
 	alert('somthing has been clicked');
 });
 //how to locate menu item
