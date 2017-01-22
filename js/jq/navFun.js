38afee042b13036150e6598be87a7cb8e163a1c2
Daniel Tilley <Tilley661>
2017-01-22 21:37:37 +0000

    debug

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -72,6 +72,8 @@ function moveToTitle2(link){
 		//queue:false
 	});
 	var z = curr.width();
+	console.log('the height of link = ' + curr.height());
+	console.log('the width of link = ' + curr.width());
 	console.log('the actual value used here = ' + ($(window).width()) - z );
 	curr.animate({'margin-left': $(window).width() - curr.width() + 'px'},1000, function(){
 					curr.addClass('new-title');
