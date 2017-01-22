a60d5d141df280f9928174ae9133a99f044df3b6
Daniel Tilley <Tilley661>
2017-01-22 18:17:31 +0000

    remove queue

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -74,8 +74,8 @@ function moveToTitle2(link){
 	//animation queue for title to nav
 	old.css({'position': 'absolute'});
 	
-	old.animate({top: '250px'},500);
-	old.animate({left: '250px'},500);
+	old.animate({top: '250px'}, duration: 500, queue:false);
+	old.animate({left: '250px'}, duration: 500, queue:false);
 	//old.queue(function(){
 	//			old.css({
 	//				'position': 'fixed',
@@ -84,7 +84,7 @@ function moveToTitle2(link){
 	//			});
 	//		});
 	
-	
+	 
 	
 	//animation queue for nav to title
 	curr.css({'position': 'relative'});
