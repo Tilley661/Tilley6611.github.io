386c3a19775af4f1f64625bbcd61bf5cc07d9b05
Tilley661 <Tilley661@mail.com>
2017-01-23 14:43:26 +0000

    add animation

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -74,7 +74,7 @@ function moveToTitle2(link){
 	//curr.css({'position': 'fixed'});
 	var currW =  $("#main-nav ul li[data-menu-index='" + link +"'] a").width(); //get width for curr title
 	var currO =  $("#main-nav ul li[data-menu-index='" + link +"'] a").offset(); //get offset for curr title
-	var currVert = (currO.top) * -1;
+	var currVert = ((currO.top) * -1)+20); //20px for padding
 	var currHoriz = ($(window).width() - currW - 50);
 	console.log('current verticle = ' + currVert + ' current horiz ' + currHoriz);
 	
@@ -97,16 +97,7 @@ function moveToTitle2(link){
 						'top':'0px',
 						'right':'0px'
 					});
-					//curr.addClass('new-title');
-					
-		
-		
-		//fix with css
-					//curr.css({
-					//	'position': 'fixed',
-					//	'top': '200px',
-					//	'left': '200px'
-		//});
+					curr.addClass('new-title');
 	});
 	//end of animation
 	
