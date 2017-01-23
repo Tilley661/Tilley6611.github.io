9affa0359c888ef245c4a0e8b65f30adfa9bd723
Tilley661 <Tilley661@mail.com>
2017-01-23 11:37:17 +0000

    correect maths

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -156,14 +156,15 @@ if (cP === 0){
 	//this means the nav is in the wrong place and no title is put foward.
 	//therfore need to set the screen
 	
-
-	console.log('position should be ' + ($(window.height)/2) - ($('#main-nav ul').height()/2))
+	var aa = $(window).height();
+	var ab = $('#main-nav ul').height();
+	console.log('position should be ' + ((aa/2) + (ab/2)) )
 	$('#main-nav ul').css({
-		'position': 'absolute',        				 	//moves with page
-		'margin-top': ($(window.height)/2) - ($('#main-nav ul').height()/2) + 'px',    	//put in center vertically
-		'margin-left': '0px'							//this should act as a home for the nav
+		'position': 'absolute',        				//moves with page
+		'margin-top': ((aa/2) + (ab/2)) + 'px',    		//put in center vertically
+		'margin-left': '0px'					//this should act as a home for the nav
 	});
-	moveToTitle2(1);  						// set to home
+	moveToTitle2(1);  					// set to home
 }
 
 
