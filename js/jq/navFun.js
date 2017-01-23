e2e350d565ba9930b7e01f7615cb7f399249bfd1
Tilley661 <Tilley661@mail.com>
2017-01-23 13:12:29 +0000

    add 50 padding

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -75,7 +75,7 @@ function moveToTitle2(link){
 	var currW =  $("#main-nav ul li[data-menu-index='" + link +"'] a").width(); //get width for curr title
 	var currO =  $("#main-nav ul li[data-menu-index='" + link +"'] a").offset(); //get offset for curr title
 	var currVert = (currO.top) * -1;
-	var currHoriz = ($(window).width() - $('#main-nav ul').width());
+	var currHoriz = ($(window).width() - currW - 50);
 	console.log('current verticle = ' + currVert + ' current horiz ' + currHoriz);
 	
 	//animation queue
