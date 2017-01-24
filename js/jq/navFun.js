7e23f97b397a881b61deb00e59dbdde144e40908
Tilley661 <Tilley661@mail.com>
2017-01-24 11:18:00 +0000

    add delay

--- js/jq/navFun.js
+++ js/jq/navFun.js
@@ -71,8 +71,8 @@ function moveToTitle2(link){
 						var nT2 = $('#title-holder-2 li');
 						var mV2 = nT2.offset().top - $('#main-nav ul li').last().offset().top;
 						console.log('debug.103 - final move = ' + mV2);
-						nT2.animate({
-							'margin-top': '200px'
+						nT2.delay(200).animate({
+							'margin-top': mV2 + 'px'
 						}, 300, function(){
 							console.log('debug.104 - animation complete')
 						});
